5c089c4f5dd83c440082d3711d6c68220ded92be
Corey O'Donnell <rcoreyodonnell@gmail.com>
2017-09-29 00:50:47 -0400

    Setup basic symlink and app install

--- scripts/common.sh
+++ scripts/common.sh
@@ -7,5 +7,32 @@
 #  ---------------------------------------------------------------------------
 
 OS=$(UNAME=$(uname); echo ${UNAME,,})
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SCRIPTS_FOLDER=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+ROOT_FOLDER=$(cd $(dirname ${SCRIPTS_FOLDER}) && pwd)
+
+# Deletes current files and symlinks dotfiles
+# $1 = dot file or directory name
+function symlink_dot_home () {
+    if [ -L ${HOME}/${1} ]; then
+        echo "~/$1 Already Exists:  Removing..."
+        rm ~/${1}
+    fi
+    ln -s ${ROOT_FOLDER}/dots/${1} ${HOME}/${1}
+    if [ -L ${HOME}/$1 ]; then
+        echo "Successfully created ${HOME}/$1"
+    else
+        echo "Error: ${HOME}/$1 not created"
+    fi
+}
+
+# Runs specified install script
+# $1 = name of install script
+function install_runner() {
+    FILE="./scripts/installs/$1.sh"
+    echo $FILE
+    if [ -f $FILE ]; then
+        source $FILE
+    else
+        echo "file does not exist"
+    fi
+}
--- setupv2.sh
+++ setupv2.sh
@@ -2,5 +2,17 @@
 
 source ./scripts/common.sh
 
-echo ${OS}
-echo ${SCRIPTS_FOLDER}
+symlink_dot_home ".bash_profile"
+symlink_dot_home ".bash_scripts"
+symlink_dot_home ".bash_aliases"
+symlink_dot_home ".vim"
+symlink_dot_home ".vimrc"
+
+if [[ ${OS} = "linux" ]]; then
+    symlink_dot_home ".fonts"
+    symlink_dot_home ".i3"
+fi
+
+install_runner git
+install_runner nvm
+# install_runner postman
